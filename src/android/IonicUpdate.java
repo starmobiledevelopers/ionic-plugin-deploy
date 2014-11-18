2c065acd1acd1c2a2dc68042b2807f348e8d0730
Joel Weirauch <joel@drifty.com>
2014-11-18 08:57:48 -0600

    Fixing some random typo's

--- src/android/IonicUpdate.java
+++ src/android/IonicUpdate.java
@@ -1,8 +1,7 @@
-packagt com.ionic.update;
+package com.ionic.update;
 
-timport android.content.Context;
-timport android.content.SharedPreferences;
-t
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.util.Log;
 
